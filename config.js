408fb867e2cbf1830636e558c257ed021e67eef2
dongphuc2255 <91251224+dongphuc2255@users.noreply.github.com>
2021-10-01 15:46:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 const CONFIG = {
-    introTitle: 'Chang à!',
+    introTitle: 'My à!',
     btnIntro: 'Dạ',
-    title: 'Phải chăng Chang đã thích Phúc ngay từ cái nhìn đầu tiên',
-    desc: 'Chang yêu Phúc đúng khumm',
+    title: 'Phải chăng MY đã thích Phúc ngay từ cái nhìn đầu tiên',
+    desc: 'My yêu Phúc đúng khumm',
     btnYes: 'Đúng<33333',
     btnNo: 'Không, nằm mơ à',
-    question:'Trên thế giới hơn 7 tỉ người mà sao Chang lại thích Phúc',
+    question:'Trên thế giới hơn 7 tỉ người mà sao My lại yêu Phúc',
     btnReply: 'Gửi cho Phúc',
     reply: 'Yêu thì yêu không yêu thì vẫn yêu <3333333',
-    mess: 'Phúc biết mà.Yêu Chang nhiềuuuuuuuuuuuuuuuuuuuuuuuuuu:',
-    messDesc: 'Tối nay 9h call ha công chúa',
+    mess: 'Phúc biết mà.Yêu My nhiềuuuuuuuuuuuuuuuuuuuuuuuuuu:',
+    messDesc: 'Tối nay Phúc cho My cục kẹo nha',
     btnAccept: 'Okiiiii lun nè <3',
     messLink: 'https://www.facebook.com/messages/t/100043090503555' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
